603e0641c4faa9f79b9ce6ea77d316f52111df77
Eks-Vadzim Simanau <eks220885@gmail.com>
2025-06-06 15:20:53 +0300

    change name in json and viteconfig

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
-    base: '/devdreamers-02/',
+    base: '/script-ninjas-project /',
     build: {
       sourcemap: true,
       rollupOptions: {
