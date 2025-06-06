11529c4199de10b72756decf729a9a68a8b0e125
Eks-Vadzim Simanau <eks220885@gmail.com>
2025-06-06 16:30:40 +0300

    add libraries

---
+++ src/js/libraries.js
@@ -0,0 +1,21 @@
+// Normalize.css
+import 'normalize.css';
+// SimpleLightbox
+import SimpleLightbox from 'simple-lightbox';
+import 'simple-lightbox/dist/simple-lightbox.min.css';
+// Axios
+import axios from 'axios';
+
+// Vanilla-tilt
+import VanillaTilt from 'vanilla-tilt';
+
+// Body-scroll-lock
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from 'body-scroll-lock';
+
+// Якщо тобі потрібні Axios або body-scroll-lock в інших файлах,
+// ти маєш їх експортувати, щоб вони були доступні:
+// export { axios, disableBodyScroll, enableBodyScroll, clearAllBodyLocks };
